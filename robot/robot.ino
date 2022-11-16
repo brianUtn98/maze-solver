78268b697a3601baaa6a20d3930a6de8b27dfc61
Brian Monroy <brian.gmonroy98@gmail.com>
2022-11-15 23:32:47 -0300

    mejoras de codigo

--- robot/robot.ino
+++ robot/robot.ino
@@ -10,7 +10,6 @@ int motorForwardRight = 3;
 int motorBackwardRight = 4;
 int motorEnableBackward = 5;
 
-
 // A0 - A5 Pines analogicos
 int frontTrigger = A0;
 int frontEcho = A1;
@@ -21,7 +20,8 @@ int leftEcho = A3;
 int rightTrigger = A4;
 int rightEcho = A5;
 
-void setup() {
+void setup()
+{
   setupForwardMotor();
 
   setupBackwardMotor();
@@ -29,11 +29,12 @@ void setup() {
   setupTriggers();
 }
 
-void loop() {
+void loop()
+{
   Serial.begin(9600);
   long frontDistance, leftDistance, rightDistance;
-    
-  //Calcular distanciad
+
+  // Calcular distanciad
 
   // Distancia frontal - Conversión según sensor
   frontDistance = getDistance(frontTrigger, frontEcho);
@@ -50,40 +51,53 @@ void loop() {
   Serial.print("Left = ");
   Serial.println(leftDistance);
   Serial.print("Right = ");
-  Serial.println(rightDistance);  
+  Serial.println(rightDistance);
   delay(50);
 
-  if (frontDistance > 20){
-
+  if (leftDistance > 20)
+  {
+    Stop();
+    delay(1000);
+    left();
+    delay(400);
     forward();
-    
-    if(isBetween(leftDistance, 10, 20)){
-      forward();
-    }
-    if(leftDistance >= 20){
-       left();
-       delay(30);
-       forward();
-    }
-    if(isBetween(leftDistance, 0, 10)){
+  }
+  else if (frontDistance > 20)
+  {
+    if (isBetween(leftDistance, 0, 10))
+    {
       right();
       delay(30);
       forward();
     }
- } 
-  
-  if(frontDistance <= 20 && rightDistance > 20){
+    else if (isBetween(rightDistance, 0, 10))
+    {
+      left();
+      delay(30);
+      forward();
+    }
+    else
+    {
+      forward();
+    }
+  }
+
+  else if (rightDistance > 20)
+  {
     Stop();
     delay(1000);
     right();
     delay(400);
+    forward();
   }
 
-  if(frontDistance <=20 && rightDistance < 20){
+  else
+  {
     Stop();
     delay(1000);
     right();
     delay(800);
+    forward();
   }
 }
 
@@ -91,29 +105,32 @@ void loop() {
 ------------------------------------------------ Setup ------------------------------------------------
 */
 
-void setupForwardMotor(){
-  pinMode(motorForwardLeft,OUTPUT);
-  pinMode(motorForwardRight,OUTPUT);   
+void setupForwardMotor()
+{
+  pinMode(motorForwardLeft, OUTPUT);
+  pinMode(motorForwardRight, OUTPUT);
   pinMode(motorEnableForward, OUTPUT);
   analogWrite(motorEnableForward, 80);
 }
 
-void setupBackwardMotor(){
-  pinMode(motorBackwardLeft,OUTPUT);   
-  pinMode(motorBackwardRight,OUTPUT);   
+void setupBackwardMotor()
+{
+  pinMode(motorBackwardLeft, OUTPUT);
+  pinMode(motorBackwardRight, OUTPUT);
   pinMode(motorEnableBackward, OUTPUT);
   analogWrite(motorEnableBackward, 88);
 }
 
-void setupTriggers(){
-  pinMode(frontTrigger,OUTPUT);
-  pinMode(frontEcho,INPUT);
+void setupTriggers()
+{
+  pinMode(frontTrigger, OUTPUT);
+  pinMode(frontEcho, INPUT);
 
-  pinMode(leftTrigger,OUTPUT);
-  pinMode(leftEcho,INPUT);
+  pinMode(leftTrigger, OUTPUT);
+  pinMode(leftEcho, INPUT);
 
-  pinMode(rightTrigger,OUTPUT);
-  pinMode(rightEcho,INPUT);
+  pinMode(rightTrigger, OUTPUT);
+  pinMode(rightEcho, INPUT);
 }
 
 /*
@@ -122,34 +139,37 @@ void setupTriggers(){
 
 void forward()
 {
-  digitalWrite(motorForwardLeft,1);
-  digitalWrite(motorBackwardLeft,0);
-  digitalWrite(motorForwardRight,1);
-  digitalWrite(motorBackwardRight,0);
+  digitalWrite(motorForwardLeft, 1);
+  digitalWrite(motorBackwardLeft, 0);
+  digitalWrite(motorForwardRight, 1);
+  digitalWrite(motorBackwardRight, 0);
   delay(1000);
 }
 
-void right(){
-  digitalWrite(motorForwardLeft,1);
-  digitalWrite(motorBackwardLeft,0);
-  digitalWrite(motorForwardRight,0);
-  digitalWrite(motorBackwardRight,1);
+void right()
+{
+  digitalWrite(motorForwardLeft, 1);
+  digitalWrite(motorBackwardLeft, 0);
+  digitalWrite(motorForwardRight, 0);
+  digitalWrite(motorBackwardRight, 1);
   delay(10);
 }
 
-void left(){
-  digitalWrite(motorForwardLeft,0);
-  digitalWrite(motorBackwardLeft,1);
-  digitalWrite(motorForwardRight,1);
-  digitalWrite(motorBackwardRight,0);
+void left()
+{
+  digitalWrite(motorForwardLeft, 0);
+  digitalWrite(motorBackwardLeft, 1);
+  digitalWrite(motorForwardRight, 1);
+  digitalWrite(motorBackwardRight, 0);
   delay(10);
 }
 
-void Stop(){
-  digitalWrite(motorForwardLeft,0);
-  digitalWrite(motorBackwardLeft,0);
-  digitalWrite(motorForwardRight,0);
-  digitalWrite(motorBackwardRight,0);
+void Stop()
+{
+  digitalWrite(motorForwardLeft, 0);
+  digitalWrite(motorBackwardLeft, 0);
+  digitalWrite(motorForwardRight, 0);
+  digitalWrite(motorBackwardRight, 0);
   delay(300);
 }
 
@@ -157,7 +177,8 @@ void Stop(){
 ------------------------------------------------ Distancia ------------------------------------------------
 */
 
-long getDistance(int trigger, int echo){
+long getDistance(int trigger, int echo)
+{
   digitalWrite(trigger, LOW);
   delayMicroseconds(2);
   digitalWrite(trigger, HIGH);
@@ -165,20 +186,14 @@ long getDistance(int trigger, int echo){
   digitalWrite(trigger, LOW);
   long duration = pulseIn(echo, HIGH);
 
-  return duration*0.034/2;
+  return duration * 0.034 / 2;
 }
 
 /*
 --------------------------------------------- Funciones útiles ---------------------------------------------
 */
 
-int isBetween(long value, long min, long max){
+int isBetween(long value, long min, long max)
+{
   return value >= min && value <= max;
 }
-
-
-  
-  
-    
-
-   
